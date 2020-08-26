590837d96a0cca0b4be5fe68fcd84107c4d2f694
wokis <kacper@kacper.se>
2020-08-26 17:51:12 +0200

    Fix for grub2 password generation
    Resolves #164
    
    Signed-off-by: wokis <kacper@kacper.se>

--- library/grub_crypt.py
+++ library/grub_crypt.py
@@ -4,9 +4,10 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-import crypt
-import random
-import string
+import random, string
+from hashlib import pbkdf2_hmac
+from binascii import hexlify
+from os import urandom
 from ansible.module_utils.basic import AnsibleModule
 
 
@@ -17,21 +18,14 @@ def gen_pass(size=16, chars=string.ascii_letters + string.digits):
 
 def gen_salt(salt):
     '''Generate a random salt.'''
-    ret = ''
     if not salt:
-        with open('/dev/urandom', 'rb') as urandom:
-            while True:
-                byte = urandom.read(1)
-                if byte in string.ascii_letters + string.digits + './':
-                    ret += byte
-                    if len(ret) == 16:
-                        break
-        return '$6$%s' % ret
-    return '$6$%s' % salt
+        return urandom(length)
+    else:
+        return salt
 
 
 def main():
-    '''Return a crypt-ed password for use with GRUB2'''
+    '''Return a pbkdf2 password for use with GRUB2'''
     module = AnsibleModule(
         argument_spec=dict(
             salt=dict(required=False, default=None),
@@ -44,13 +38,18 @@ def main():
         )
 
     )
+    
+    algo = 'sha512'
+    iterCount = 10000
+    
     salt = module.params['salt']
     password = module.params['password']
     if password == 'random':
         password = gen_pass()
     sha512_salt = gen_salt(salt)
-    salted_pass = crypt.crypt(password, sha512_salt)
-    module.exit_json(changed=False, passhash=salted_pass)
+    salted_pass = pbkdf2_hmac(algo, password, sha512_salt, iterCount)
+    pbkdf2_pass = 'grub.pbkdf2.{}.{}.{}.{}'.format(algo, iterCount, hexlify(sha512_salt).upper(), hexlify(salted_pass).upper())
+    module.exit_json(changed=False, passhash=pbkdf2_pass)
 
 
 if __name__ == '__main__':
